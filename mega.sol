fabbd351b13ab653b17314c96aea2a3cf98d48fa
gigiraffaa <58345449+gigiraffaa@users.noreply.github.com>
2020-01-20 01:28:00 +0000

    initial implementation v0.01(G)

--- mega.sol
+++ mega.sol
@@ -1,71 +1,97 @@
 pragma solidity >=0.4.22 <0.6.0;
+pragma experimental ABIEncoderV2;
+
 contract MEGA{
-    // string token_name = "ETS tokens";
-    // string token_code = "ETS";
-
-    //static variable : total initial cap
-
-    //empty mapping / dictionary : user id (blockchain address) to the number of allowances and number of emissions
-
 
     uint256 total_cap = 10000;
-    mapping(address => uint256) public user_balance;
+    uint total_actors = 20;
+    uint public usersCount = 0;
+    uint public auctionCount = 0;
+    
+    mapping(address => Person) registered_users;
+    mapping(uint => Auction) public active_auctions;
 
-    address owner;
+    struct Person{
+        address personAddress;
+        uint tokensWallet;
+        uint emissionsTotal;
+        bool isActive;
+    }
+    
+    function createPerson (address _personAddress) private returns (Person memory){
+        uint tokenAllocation = total_cap / total_actors;
+        return Person({ personAddress: _personAddress, tokensWallet: tokenAllocation, emissionsTotal: 0, isActive: true});
+    }
+        
+    function register() public{
+        require(registered_users[msg.sender].isActive == false);
+        Person memory newUser = createPerson(msg.sender);
+        registered_users[msg.sender] = newUser;
+        usersCount += 1;
+    }
+    
+    struct Auction{
+        uint auctionID;
+        uint numTokens;
+        uint minPrice;
+        uint increment; //default set to 1 - verify (lawyers?)
+        address sellerID;
+        bool isActive;
+        
+        uint numBids; //default set to 0
+        
+        uint highestCurrentBid;
+        uint highestMaxBid;
+        Person highestBidder;
+        
+        // endTime : timestamp / endblock
+        // payersHistory : list (of tuples?)
+        
+    }
+    
 
-    constructor() public {
-        // if (user_balance[owner] == 0){
-        //     user_balance[owner] =  200;
-        // }
-        // here assign each user with the initial number of tokens
-        //
+    function sellTokens(uint _amount, uint _minPrice) public {
+        auctionCount += 1;
+        Auction memory newAuction = Auction({ 
+                                            auctionID: auctionCount, 
+                                            numTokens: _amount, 
+                                            minPrice: _minPrice, 
+                                            increment: 1, 
+                                            sellerID: msg.sender, 
+                                            isActive: true, 
+                                            numBids: 0,
+                                            highestCurrentBid: _minPrice,
+                                            highestMaxBid: 0,
+                                            highestBidder: Person(0x0000000000000000000000000000000000000000, 0, 0, false)
+        });
+        active_auctions[auctionCount] = newAuction;
+    }
+    
+    
+    
+    
+    function buyTokens(uint _auctionID, uint _maxBid) public {
+        //check if requestor has enough ether
+        
+        // SHALL WE START FROM MINIMUM ? 
+        
+        //check if maxBid > highestCurrentBid
+        if (_maxBid > active_auctions[_auctionID].highestCurrentBid){
+            if (_maxBid > active_auctions[_auctionID].highestMaxBid){
+                active_auctions[_auctionID].highestCurrentBid = active_auctions[_auctionID].highestMaxBid + active_auctions[_auctionID].increment;
+                active_auctions[_auctionID].highestMaxBid = _maxBid;
+                active_auctions[_auctionID].highestBidder = registered_users[msg.sender];
+                active_auctions[_auctionID].numBids += 1;
+            }
+            else {
+                active_auctions[_auctionID].highestCurrentBid = _maxBid + active_auctions[_auctionID].increment;
+            }
+        }
+        else {}
     }
 
-    //function : see total cap
-    function totalEmissionsCap() view public returns (uint256){
-        return total_cap;
-    }
 
-    //function : see user balance
-
-    //function : see user ether
-
-    //function : see user emissions
-
-    //fucntion : sell allowance
-
-    //function : see all auctions
-
-    //function : cancel transaction
-
-    //function : (ADMIN) finalise year
-
-    //function : (ADMIN) set total cap
-
-    //function : (ADMIN) set decrease rate
-
-
-
-
-    // function balanceOf(address _user) view public returns (uint256){
-    //     return user_balance[_user];
-    // }
-
-
-    // function transfer(address _to, uint _value) public returns (bool){
-    //     if (user_balance[owner] >= _value){
-    //         user_balance[owner] -= _value;
-    //         user_balance[_to] += _value;
-    //         emit Transfer(owner, _to, _value);
-    //     }
-    //     else {
-    //         emit NotEnoughFunds(owner, _to, _value, user_balance[owner]);
-    //     }
-
-    // }
-
-    // event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    // event NotEnoughFunds(address indexed _from, address indexed _to, uint256 _value, uint256 _userbalance);
+    
 
 
 }
