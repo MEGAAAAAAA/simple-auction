8a1c18349ed1262e0f9822b3238f066919da76d3
gigiraffaa <58345449+gigiraffaa@users.noreply.github.com>
2020-01-30 10:46:00 +0000

    test change
    added some comments to test the update

--- mega.sol
+++ mega.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 
 /*
 
-MEGA.sol v0.012 (Gosia's changes on 20/1/20 4:30pm)
+MEGA.sol v0.014 (Gosia's changes on 30/1/20 10:45am)
 
 Contract MEGA is intended to assist with allowance trading within the European Union Emissions Trading Scheme (EU ETS).
 
@@ -40,7 +40,7 @@ contract MEGA{
 
     //Function to register a new user
     function register() public{
-        require(registered_users[msg.sender].isActive == false); //check if users not registered yet
+        require(registered_users[msg.sender].isActive == false, "You are already registered"); //check if users not registered yet
         if (usersCount <= total_actors){ //check if there is an available actor slot
             Person memory newUser = createPerson(msg.sender); //generate a new user
             registered_users[msg.sender] = newUser; //add user to registered_users list
@@ -67,9 +67,11 @@ contract MEGA{
 
     }
 
+
+
     //Function to place tokens on auction
     function sellTokens(uint _amount, uint _minPrice) public {
-        require(_amount != 0); //require the auction not to be empty
+        require(_amount != 0, "Cannot sell empty auction."); //require the auction not to be empty
         auctionCount += 1;
         //Instantiating a new auction with specified amount and minimum price
         Auction memory newAuction = Auction({
@@ -87,24 +89,31 @@ contract MEGA{
     }
 
 
-    //Function to place a bid on the auction
+
+
+    //Function to place a bid on the auction THIS NEEDS TO BE UPDATED :)
     function buyTokens(uint _auctionID, uint _maxBid) public {
         //add condition to check if requestor has enough ether
-        require(msg.sender.balance > _maxBid); //require the buyer to have enough ether funds to cover the transaction
+        require(msg.sender.balance > _maxBid, "You have insufficient funds."); //require the buyer to have enough ether funds to cover the transaction
+        //I think eventually we will need a requirement here that the auction addressed is active
+        require(_maxBid > active_auctions[_auctionID].highestCurrentBid, "You cannot bid less than the current highest bid."); //require the amount that the buyer is offering is greater than the current highest bid.
 
-        if (_maxBid > active_auctions[_auctionID].highestCurrentBid){
-            if (_maxBid > active_auctions[_auctionID].highestMaxBid){
-                active_auctions[_auctionID].highestCurrentBid = active_auctions[_auctionID].highestMaxBid + active_auctions[_auctionID].increment;
-                active_auctions[_auctionID].highestMaxBid = _maxBid;
-                active_auctions[_auctionID].highestBidder = registered_users[msg.sender];
-                active_auctions[_auctionID].numBids += 1;
-            }
-            else {
-                active_auctions[_auctionID].highestCurrentBid = _maxBid + active_auctions[_auctionID].increment;
-            }
+
+        if (_maxBid > active_auctions[_auctionID].highestMaxBid){
+            active_auctions[_auctionID].highestCurrentBid = active_auctions[_auctionID].highestMaxBid + active_auctions[_auctionID].increment;//min((active_auctions[_auctionID].highestMaxBid + active_auctions[_auctionID].increment), _maxBid), but this won't compile;
+            active_auctions[_auctionID].highestMaxBid = _maxBid;
+            active_auctions[_auctionID].highestBidder = registered_users[msg.sender];
+            active_auctions[_auctionID].numBids += 1;
         }
-        else {}
+        else if (_maxBid == active_auctions[_auctionID].highestMaxBid){
+            active_auctions[_auctionID].highestCurrentBid = _maxBid;
+        }
+        else{
+            active_auctions[_auctionID].highestCurrentBid = _maxBid + active_auctions[_auctionID].increment; //min((_maxBid + active_auctions[_auctionID].increment), active_auctions[_auctionID].highestMaxBid), but this won't compile;
+        }
     }
+
+
 
     //TEST function to check the current user ether balance - to be removed later
     function getBalance(address _addressToCheck) public returns (uint){
