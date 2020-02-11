280fd7635f6066abede1b7fd6968231a3303521a
gigiraffaa <58345449+gigiraffaa@users.noreply.github.com>
2020-02-11 19:40:01 +0000

    cleaner

--- mega.sol
+++ mega.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 
 /*
 
-MEGA.sol v0.013 (Annazita's changes on 20/1/20 7:08pm)
+MEGA.sol v0.015 (Gosia's changes on 11/2/20 5:00pm)
 
 Contract MEGA is intended to assist with allowance trading within the European Union Emissions Trading Scheme (EU ETS).
 
@@ -12,30 +12,33 @@ Contract MEGA is intended to assist with allowance trading within the European U
 
 contract MEGA{
 
-
     //Global variables and data structures used in the contract
     uint256 total_cap = 10000; //the initial number of total allowance tokens in the contract
     uint total_actors = 20; //the initial number of actors in the contract
     uint public frozenTokens = 0;
-    uint public usersCount = 0; //number of registered users (OPTIONAL - for test purposes)
-    uint public auctionCount = 0; //number of currently active auctions (OPTIONAL - for test purposes)
+    uint usersCount = 0; //number of registered users (OPTIONAL - for test purposes)
+    uint auctionCount = 0; //number of currently active auctions (OPTIONAL - for test purposes)
     uint public theBalanceToCheckEther = 0; //balance that is being checked (OPTIONAL - for test purposes - REMOVE LATER)
-    uint public theBalanceToCheckTokens = 0;
-    address payable owner;
+    
+    uint yearEndTime = 0; //time when the year is over
+    uint daysInYear = 0; //Number of days in this year
+    bool yearDistribution = false; //have the tokens been distributed for this year
+    address public owner;
 
 
-    mapping(address => Person) registered_users; //a mapping with registered users and their corresponding addresses
+    mapping(address => Person) public registered_users; //a mapping with registered users and their corresponding addresses
     mapping(uint => Auction) public active_auctions; //a mapping with all currently active auctions and their corresponding IDs
-    mapping(uint => uint) public depositors_tokens;
+    mapping(uint => uint) public deposited_tokens;
 
 
     constructor() public {
-        // State variables are accessed via their name
-        // and not via e.g. this.owner. This also applies
-        // to functions and especially in the constructors,
-        // you can only call them like that ("internally"),
-        // because the contract itself does not exist yet.
         owner = msg.sender;
+        yearEndTime = now + 365 days; // this is variable and I think this is a problem
+    }
+
+     modifier _ownerOnly(){
+      require(msg.sender == owner);
+      _;
     }
 
     //Struct defining a person (user) in the contract
@@ -61,6 +64,18 @@ contract MEGA{
         }
     }
 
+    function endTheYear(uint _daysInNextYear) private _ownerOnly{
+        require(now > yearEndTime, 'The year has not ended yet');
+        daysInYear = _daysInNextYear;
+        yearDistribution = false;
+    }
+
+    function distribute() private _ownerOnly{
+        require(yearDistribution == false, 'Already distibuted this year');
+        //send out tokens to all active users
+        yearDistribution = true;
+    }
+    
     //Struct defining an auction, I got rid of increment and highestMaxBid
     struct Auction{
         uint auctionID; //unique auction identifier
@@ -70,52 +85,82 @@ contract MEGA{
         bool isActive; //flag for active auction
         uint numBids; //number of current bidders in the auction
         uint highestCurrentBid; //value of the current highest bid
+        uint auctionEnd; //The time after which the auction can be ended
         Person highestBidder; //Person instance of the highest bidder
 
         mapping(address => uint) bidders; //mapping with amount of money paid to bid
 
-        //Implement the below in the future:
-        // endTime : timestamp / endblock
-        // payersHistory : list (of tuples?)
-
     }
 
     //Function to place tokens on auction
-    function sellTokens(uint _amount, uint _minPrice) public {
+    function sellTokens(uint _amount, uint _minPrice, uint _biddingTime) public {
         //ACTION REQUIRED : needs to check if has enough tokens to sell
-
+        //
 
         require(_amount != 0, "Cannot sell empty auction.");//require the auction not to be empty
         auctionCount += 1;
+        
         //Instantiating a new auction with specified amount and minimum price
         Auction memory newAuction = Auction({
                                             auctionID: auctionCount,
                                             numTokens: _amount,
-                                            minPrice: _minPrice,
+                                            minPrice: _minPrice * 1000000000000000000,
                                             sellerID: msg.sender,
                                             isActive: true,
                                             numBids: 0,
                                             highestCurrentBid: 0,
-                                            highestBidder: Person(0x0000000000000000000000000000000000000000, 0, false)});
+                                            auctionEnd: now + _biddingTime, // * 1 hours,
+                                            highestBidder: Person(0x0000000000000000000000000000000000000000, 0, false)
+                                            //bidders: 
+        });
+                                            
         active_auctions[auctionCount] = newAuction; //adding auction to the mapping of currently active functions
+        
+        
+        frozenTokens += _amount;
+        registered_users[msg.sender].tokensWallet -= _amount;
+        deposited_tokens[auctionCount] = _amount;
     }
 
 
     //Function to place a bid on the auction
-    function buyTokens(uint _auctionID, uint _userBid) public {
-        //add condition to check if requestor has enough ether
-        require(msg.sender.balance > _userBid, "You have insufficient funds."); //require the buyer to have enough ether funds to cover the transaction
-        //I think eventually we will need a requirement here that the auction addressed is active
-        require(_userBid > active_auctions[_auctionID].highestCurrentBid, "You cannot bid less than or equal to the current highest bid."); //require the amount that the buyer is offering is greater than the current highest bid.
-        require(_userBid >= active_auctions[_auctionID].minPrice, "The seller is not willing to sell at this price"); //require the bidder to bid at least the minPrice
+    function buyTokens(uint _auctionID) public payable {
+        
+        require(msg.sender.balance > msg.value, "You have insufficient funds."); //require the buyer to have enough ether funds to cover the transaction
+        require(now <= active_auctions[_auctionID].auctionEnd, 'The ending time for this auction has been reached');//require that the end time for the auction has not already happened
+        require(active_auctions[_auctionID].isActive, 'This auction is not active');//requrie that the auction is active
+        require(msg.value > active_auctions[_auctionID].highestCurrentBid, "You cannot bid less than or equal to the current highest bid."); //require the amount that the buyer is offering is greater than the current highest bid.
+        require(msg.value >= active_auctions[_auctionID].minPrice, "The seller is not willing to sell at this price"); //require the bidder to bid at least the minPrice
 
-        active_auctions[_auctionID].highestCurrentBid = _userBid;//min((active_auctions[_auctionID].highestMaxBid + active_auctions[_auctionID].increment), _maxBid), but this won't compile;
+
+        //CONDITION IF BIDDING FOR THE FIRST TIME
+        active_auctions[_auctionID].highestCurrentBid = msg.value;//min((active_auctions[_auctionID].highestMaxBid + active_auctions[_auctionID].increment), _maxBid), but this won't compile;
         active_auctions[_auctionID].highestBidder = registered_users[msg.sender];
         active_auctions[_auctionID].numBids += 1;
         // msg.value = _userBid;
         // depositFunds(_auctionID, msg.sender);
+        active_auctions[_auctionID].bidders[msg.sender] = msg.value; 
+        
+        //CONDITION IF BIDDED ALREADY - INCREMENT
+        // active_auctions[_auctionID].bidders[_bidderAddress] = msg.value; /
+        
     }
 
+
+
+    function endAuction(uint _auctionID) public{
+        require(now > active_auctions[_auctionID].auctionEnd, 'The ending time for this auction has not yet been reached');//require that the end time for the auction has already happened
+        active_auctions[_auctionID].isActive = false;
+        //send back ether to all non-winning bidders
+        //add the amount of tokens to the winning bidder
+    }
+
+    function cancelAuction(uint _auctionID) public{
+        require(msg.sender == active_auctions[_auctionID].sellerID, 'Only the seller can cancel an auction' ); //require that only the seller can cancel an auction
+        active_auctions[_auctionID].isActive = false;
+        //send back ether to all bidders
+        //send back tokens to seller
+    }
 
     //TEST function to check the current user ether balance - to be removed later
     function getBalanceEther(address _addressToCheck) public returns (uint){
@@ -123,31 +168,10 @@ contract MEGA{
         return theBalanceToCheckEther;
     }
 
-
-    //Function to check the balance of tokens of a particular user
-    function getBalanceTokens(address _addressToCheck) public returns (uint){
-        theBalanceToCheckTokens = registered_users[_addressToCheck].tokensWallet;
-        return theBalanceToCheckTokens;
+    function contractEther() public {
+        theBalanceToCheckEther = getBalanceEther(address(this));
     }
 
 
-    function depositFunds(uint _auctionID, address _bidderAddress) external payable { //auction ID, bidder address
-        owner.transfer(msg.value); // transfer from bidder to ^^account;
-        active_auctions[_auctionID].bidders[_bidderAddress] = msg.value; // add to the list of mappings of bidders in a specific auction
-
-        // error message
-    }
-
-
-
-    // function depositTokens(address _depositorT, uint _amountT, uint _auctionID){
-    //     frozenTokens += _amountT;
-    //     registered_users[_depositorT].tokensWallet -= _amountT;
-    // }
-
-
-    // function withdrawTokens(address _collectorT, uint _amountT){
-
-    // }
 
 }
